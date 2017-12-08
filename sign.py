efbbaf0d2214987d0b00a5dfaa5c2cf03bcdeb23
Crab <crab@CrabpCrap.local>
2017-12-08 02:55:42 +0200

    reworked sign.py

--- sign.py
+++ sign.py
@@ -5,10 +5,18 @@ from Crypto.Signature import PKCS1_v1_5
 
 key = RSA.generate(1024)
 
-kf = open('publick.pem', 'wb')
+kf = open('closed.pem', 'wb')
 kf.write(key.exportKey('PEM'))
 kf.close()
 
-with open('message.txt', 'r') as f:
-    h = SHA.new(f)
+with open('message.txt', 'rb') as f:
+    message = f.read()
+    h = SHA.new(message)
 
+signature = PKCS1_v1_5.new(key).sign(h)
+
+
+pubkey = key.publickey()
+
+with open('pubkey.pem', 'wb') as f:
+    f.write(pubkey.exportKey('PEM'))
